2daea2e587e96f334e617ab8ad5bc9e98073d1c4
ArchieGertsman <arkadiygertsman@gmail.com>
2023-10-25 15:45:23 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Archie Gertsman",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Machine Learning Engineer",
+    bio: "I like ML.",
+    email: "arkadiygertsman@gmail.com",
+    linkedin: "arkadiy-gertsman",
+    github: "ArchieGertsman"
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `spark-sched-sim`,
+      href: "https://github.com/ArchieGertsman/spark-sched-sim",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Archie's Blog",
+    description: "welcome to my blog!",
   },
 
   // CONFIG configration (required)
@@ -47,7 +46,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
